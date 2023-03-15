d1fb40cbfba6498315ddc644c92734040652ace9
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-03-15 17:56:40 +0530

    Calculation of Cab Invoice Generator Program

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator Program");
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoice.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
         }
     }
-}
+    }
+
